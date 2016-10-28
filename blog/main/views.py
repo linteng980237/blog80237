24269e05572ec73f12178d926c09734ab81cecb4
linteng980237 <alal19970603@gmail.com>
2016-10-28 10:08:13 +0800

    first push

---
+++ blog/main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import datetime
+from django.core.paginator import Page
+
+
+def main(request):
+    '''
+    Render the main page
+    '''
+    now = datetime.datetime.now()
+    context = {'like':'Django 很棒','now':now}
+    return render(request, 'main/main.html', context)
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')    
+
+def contact(request):
+    '''
+    Render the contact Page
+    '''
+    return render(request, 'main/contact.html')
