abb7aa5de8be454f9b48d5442b3c30511c0566d3
linteng980237 <linteng@gmail.com>
2016-11-04 09:32:57 +0800

    create models

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    content = models.TextField()
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    article = models.ForeignKey(Article)
+    content = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.article.title + '-' + str(self.id)
